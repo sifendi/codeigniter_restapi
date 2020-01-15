89ffdd03c502902bdfde6f5be237e181858b9431
afendi <afendiajip94@gmail.com>
2020-01-15 09:11:17 +0700

    new push for presensi get and post 2

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('standartapi');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array('jwt');
+$autoload['config'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Users','Resources', 'Roles');
+$autoload['model'] = array();
